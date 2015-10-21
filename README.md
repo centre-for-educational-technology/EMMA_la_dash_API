2df0ee8361b238537272c31b24862d49e90f4c4c
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-10-21 20:59:12 +0300

    Fix for cache logic, was never refreshed.
    Also made storage location configurable, though it still defaults to
    storage/ within the root catalog. Updated documentation a bit.

--- README.md
+++ README.md
@@ -10,11 +10,15 @@ A backend API used by Emma Dashboard.
   - Please note that there is a need to provide suitable configurations yourself
   - Please note the EDB_APP_PATH, just leave it empty in case of root installation,
   otherwise it should match RewriteBase, just the ending slash needs to be removed
+3. Make sure that *data/* catalog is writable for the apache
 
 # API
 
 * course/ID/participants - responds with participants data
 * course/ID/activity_stream - responds with stream data
-* course/ID/overview - responds with overview Data
+* course/ID/overview - responds with overview data
+* course/ID/lessons - responds with lessons and units data (just structural)
+* course/ID/lesson/ID/unit/ID - responds with unit data
+* course/ID/sna - responds with network data (Sigma.js friendly)
 
 The API is currently tailored to suit the needs of certain parts of the UI
--- classes/EmmaDashboardStorage.php
+++ classes/EmmaDashboardStorage.php
@@ -6,13 +6,17 @@
  * Could run into issues in future.
  */
 
+// XXX Need to make sure that directory is writable (either throw an error or
+// add that to the installation steps)
+
 class EmmaDashboardStorage {
   public function __construct() {
-    $this->storage = __DIR__ . '/../storage';
+    $this->storage = EDB_STORAGE_PATH;
   }
 
   public function buildFileLocation($course_id, $file_name) {
     if ( !file_exists($this->storage . '/' . $course_id) ) {
+      // XXX This could fail silently
       mkdir($this->storage . '/' . $course_id);
     }
 
@@ -25,7 +29,7 @@ class EmmaDashboardStorage {
     if ( file_exists($file_location) ) {
       $modified_time = filemtime($file_location);
 
-      if ( $modified_time && ($modified_time - time()) <= 60 * 60 ) {
+      if ( $modified_time && ( (time() - $modified_time) <= 60 * 60 ) ) {
         return file_get_contents($file_location);
       }
     }
@@ -36,6 +40,7 @@ class EmmaDashboardStorage {
   public function createOrUpdateFile($course_id, $file_name, $data) {
     $file_location = $this->buildFileLocation($course_id, $file_name);
 
+    // XXX This could fail silently
     $handle = fopen($file_location, 'w');
     fwrite($handle, $data);
     fclose($handle);
