b88a7dc80e6fe1f0731801515df555f433f6a874
Pjotr Savitski <pjotr.savitski@gmail.com>
2016-01-27 15:44:21 +0200

    Added a check for service code and content-type.
    In case service has some issues and responds with errors or the
    content-type of a response is not application/json. Then the response is
    not processed any further and error is returned. This seems reasonable,
    but would still require some testing before releasing.

--- classes/EmmaDashboardServiceCaller.php
+++ classes/EmmaDashboardServiceCaller.php
@@ -43,7 +43,15 @@ class EmmaDashboardServiceCaller {
       throw new EmmaDashboardServiceException('Service Error, please contact Administrator.');
     }
 
+    $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $conetnt_type = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
+
     curl_close($curl);
+
+    if ( $http_code !== 200 || strtolower($content_type) !== 'application/json' ) {
+      error_log('Web Service Responded With Error: ' $http_code . ' and content-type ' . $content_type );
+      throw new EmmaDashboardServiceException('Service Error, please contact Administrator.');
+    }
 
     $this->examineResponse($result);
 
